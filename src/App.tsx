6276ec15771c809c4b304dfdf9b0b9b6ec6b71fd
nad388 <nad388@yandex.ru>
2022-11-28 20:38:52 +0600

    added first component. Typisation props. Interface CardProps

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './components/Card';
+
+const App = () => {
+  return (
+    <div>
+      <Card width='200px' height='200px'>
+        <button>Press me</button>
+        <h5>Doppelganger!!!</h5>
+      </Card>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+interface CardProps {
+  width?: string;
+  height?: string;
+  children?: React.ReactNode;
+}
+
+const Card = ({ width, height, children }: CardProps) => {
+  return (
+    <div style={{ width, height, border: '1px solid gray' }}>{children}</div>
+  );
+};
+
+export default Card;
