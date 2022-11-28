48b28837918fb37f6124e674bf5a3777a92d6c43
nad388 <nad388@yandex.ru>
2022-11-28 20:58:06 +0600

    added React.FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import Card from './components/Card';
+import Card, { CardVariant } from './components/Card';
 
 const App = () => {
   return (
     <div>
-      <Card width='200px' height='200px'>
+      <Card
+        onClick={(num) => console.log('click!!!', num)}
+        variant={CardVariant.primary}
+        width='200px'
+        height='200px'
+      >
         <button>Press me</button>
         <h5>Doppelganger!!!</h5>
       </Card>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,32 @@
-import * as React from 'react';
+import React, { FC, useState } from 'react';
+
+export enum CardVariant {
+  outline = 'outline',
+  primary = 'primary',
+}
 
 interface CardProps {
   width?: string;
   height?: string;
+  variant: CardVariant;
+  onClick: (num: number) => void;
   children?: React.ReactNode;
 }
 
-const Card = ({ width, height, children }: CardProps) => {
+const Card: FC<CardProps> = ({ width, height, variant, onClick, children }) => {
+  const [state, setState] = useState(0);
   return (
-    <div style={{ width, height, border: '1px solid gray' }}>{children}</div>
+    <div
+      style={{
+        width,
+        height,
+        border: variant === CardVariant.outline ? '1px solid gray' : 'none',
+        background: variant === CardVariant.primary ? 'yellow' : 'blue',
+      }}
+      onClick={() => onClick(state)}
+    >
+      {children}
+    </div>
   );
 };
 
