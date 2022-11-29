9a2ca6cc89935a98664c2990e92b5f4ce0be0daf
nad388 <nad388@yandex.ru>
2022-11-29 12:58:12 +0600

    added typing useRef

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -1,15 +1,16 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useRef, useState } from 'react';
 
 const EventsExample: FC = () => {
   const [value, setValue] = useState<string>('');
   const [isDrag, setIsDrag] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
   const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log(value);
+    console.log(inputRef.current?.value);
   };
 
   const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
@@ -32,7 +33,13 @@ const EventsExample: FC = () => {
 
   return (
     <div>
-      <input type='text' value={value} onChange={changeHandler} />
+      <input
+        type='text'
+        value={value}
+        onChange={changeHandler}
+        placeholder='Управляемый'
+      />
+      <input type='text' ref={inputRef} placeholder='Неуправляемый' />
       <button onClick={clickHandler}>PRESS ME</button>
       <div
         draggable
